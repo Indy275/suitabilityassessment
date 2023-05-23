c5b3b1145cf4f8558d701aff5e901696fd168539
Indy Dolmans <indy.dolmans@nelen-schuurmans.nl>
2023-05-23 11:12:23 +0200

    fixed issue with positive label count

--- plotting/plot_data.py
+++ plotting/plot_data.py
@@ -26,14 +26,14 @@ def plot_y(y_train, y_test, bg_train, bg_test, train_w, train_h, test_w, test_h,
     # cmap.set_bad('black')
     ax[0, 0].imshow(y_train.reshape((train_h, train_w)), cmap='Set1_r')
     posperc = len(y_train[y_train != 0]) / len(y_train) * 100
-    ax[0, 0].set_title("Train labels: {} positive ({:.3f}%)".format(int(np.nansum(y_train)), posperc))
+    ax[0, 0].set_title("Train labels: {} positive ({:.3f}%)".format(len(y_train[y_train != 0]), posperc))
 
     ax[0, 1].imshow(bg_train, cmap='Set3')
     ax[0, 1].set_title("Train area")
 
     ax[1, 0].imshow(y_test.reshape((test_h, test_w)), cmap='Set1_r')
     posperc = len(y_test[y_test != 0]) / len(y_test) * 100
-    ax[1, 0].set_title("Test labels: {} positive ({:.3f}%)".format(int(np.nansum(y_test)), posperc))
+    ax[1, 0].set_title("Test labels: {} positive ({:.3f}%)".format(len(y_test[y_test != 0]), posperc))
 
     ax[1, 1].imshow(bg_test, cmap='Set3')
     ax[1, 1].set_title("Test area")
