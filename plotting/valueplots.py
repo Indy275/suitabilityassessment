dfe4058b1b2ae029963a8686257e35fff386fcf8
Indy Dolmans <indy.dolmans@nelen-schuurmans.nl>
2023-06-14 11:20:47 +0200

    plot histograms

---
+++ plotting/valueplots.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import configparser
+import numpy as np
+import os
+from joblib import load
+
+config = configparser.ConfigParser()
+parent = os.path.dirname
+config.read(os.path.join(parent(parent(__file__)), 'config.ini'))
+
+data_url = config['DEFAULT']['data_url']
+
+# Load the dataframe from CSV or any other data source
+df = pd.read_csv(data_url + '/' + 'noordholland' + '/' + 'expert_ref' + ".csv")
+ss = load(data_url + '/' + 'noordholland' + '/' + "scaler.joblib")
+df2 = df.to_numpy()
+df_orig = ss.inverse_transform(df2[:, 2:-1])
+# Create a bar graph for each column
+for i, column in enumerate(df.columns):
+    plt.figure()
+    print(np.nanmax(df[column]), np.nanargmax(df[column]))
+    print(df_orig[np.nanargmax(df[column]),:])
+
+    plt.hist(df[column], bins=20)
+    plt.title(column)
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.show()
