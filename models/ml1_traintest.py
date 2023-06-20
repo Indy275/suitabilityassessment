040ffccf800cabfd586bb54492bc8e17d5fcaa0d
Indy Dolmans <indy.dolmans@nelen-schuurmans.nl>
2023-06-20 12:28:43 +0200

    y_pred now consists of lon, lat tuple as well

--- models/ml1_traintest.py
+++ models/ml1_traintest.py
@@ -21,28 +21,31 @@ plot_feature_importance = int(config['PLOTTING']['plot_feature_importance'])
 
 def save_imp(imp, names, model, modifier):
     imp = np.abs(imp)
-    new_imp = imp/np.max(imp)
+    new_imp = imp / np.max(imp)
     impFrame = pd.DataFrame(data=dict({'feature': names, 'importance': new_imp}))
     impFrame.to_csv(data_url + '/' + modifier + "/" + model + "_fimp.csv", index=False)
 
 
 def train_predict(X_train, y_train, X_test, y_test, test_w, test_h, model, modifier, col_names, plot_fimp=0):
     n_feats = X_train.shape[-1]
-    train_vals = y_train != 0
-    X_train = X_train[train_vals]
+    X_train = X_train[y_train != 0]
     X_train = X_train.reshape((-1, n_feats))
-    X_train = X_train[:, 2:]  # Exclude lon, lat
-    y_train = y_train[train_vals]
+    # X_train = X_train[:, 2:]  # Exclude lon, lat
+    y_train = y_train[y_train != 0]
 
     test_vals = y_test != 0
     test_nans = np.isnan(X_test).any(axis=1)
+    X_test2 = np.copy(X_test)
     X_test = X_test[~test_nans]
     X_test = X_test.reshape((-1, n_feats))
-    X_test = X_test[:, 2:]  # Exclude lon, lat
+    # X_test = X_test[:, 2:]  # Exclude lon, lat
 
     col_names = col_names[2:-1]
-    y_preds = np.zeros(y_test.shape)
-    y_preds[test_nans] = np.nan
+    y_preds = np.zeros((y_test.shape[0],3))  # (:, lon, lat, y_test)
+    y_preds[:, 0] = X_test2[:, 0]
+    y_preds[:, 1] = X_test2[:, 1]
+
+    y_preds[test_nans, :] = np.nan
 
     if model == 'gbr':
         reg = GradientBoostingRegressor()
@@ -58,7 +61,7 @@ def train_predict(X_train, y_train, X_test, y_test, test_w, test_h, model, modif
             save_imp(reg.feature_importances_, col_names, model, modifier)
     elif model == 'svm':
         posfeat = [feat for feat, lab in zip(X_train, y_train) if lab > 0]
-        kernel = 'linear'  # 'linear'
+        kernel = 'linear'  # 'linear'  'rbf'
         svm = OneClassSVM(kernel=kernel)
         svm.fit(X=posfeat)
         y_pred = svm.predict(X_test)
@@ -70,20 +73,19 @@ def train_predict(X_train, y_train, X_test, y_test, test_w, test_h, model, modif
         print("Invalid model; should be one of ['gbr','svm']")
         y_pred = np.repeat(0, y_preds[~test_nans[:, 0]].shape)
 
-    y_preds[~test_nans] = y_pred
-    if sum(test_vals)>0:  # Only perform MSE test if we have test labels
-        mse = mean_squared_error(y_test[test_vals], y_preds[test_vals])
-        print("Test MSE: {:.4f}".format(mse))
+    y_preds[~test_nans, -1] = y_pred
     pd.DataFrame(y_preds).to_csv(data_url + '/' + modifier + "/" + model + "_pred.csv")
     if plot_pred:
         plot.plot_prediction(y_preds, test_h)
         if model == 'svm':
-            y_preds[~test_nans[:, 0]] = y_pred2
+            y_preds[~test_nans, -1] = y_pred2
             plot.plot_prediction(y_preds, test_h)
+    if sum(test_vals) > 0:  # Only perform MSE test if we have test labels
+        mse = mean_squared_error(y_test[test_vals], y_preds[test_vals, -1])
+        print("Test MSE: {:.4f}".format(mse))
 
 
 def run_model(train_mod, test_mod, model, train_w, train_h, test_w, test_h, ref_std):
-
     X_train, y_train, X_train_orig, train_col_names = load_data.load_xy(train_mod, ref_std)
     X_test, y_test, X_test_orig, test_col_names = load_data.load_xy(test_mod, ref_std)
     assert train_col_names == test_col_names
