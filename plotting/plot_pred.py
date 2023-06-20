60deb8afe89a9cc30de2332b86fc5e7912996cb6
Indy Dolmans <indy.dolmans@nelen-schuurmans.nl>
2023-06-20 12:30:54 +0200

    more consistent with plotdata

--- plotting/plot_pred.py
+++ plotting/plot_pred.py
@@ -5,6 +5,16 @@ from textwrap import wrap
 import matplotlib as mpl
 
 
+def set_colmap(img):
+    colmap = LinearSegmentedColormap.from_list('colmap2', [[1, 0, 0, 1], [145 / 255, 210 / 255, 80 / 255, 1]])
+    try:
+        plt.register_cmap(cmap=colmap)
+    except:
+        pass
+    plt.set_cmap(colmap)
+    img.cmap.set_bad('tab:blue')
+
+
 def plot_f_importances(coef, names):
     imp = coef
     imp = np.abs(imp)
@@ -16,29 +26,30 @@ def plot_f_importances(coef, names):
     plt.show()
 
 
-def plot_prediction(y_pred, height):
-    colmap = LinearSegmentedColormap.from_list('colmap2', [[1, 0, 0, 1], [145 / 255, 210 / 255, 80 / 255, 1]])
-    try:
-        plt.register_cmap(cmap=colmap)
-    except:
-        pass
+def plot_prediction(y_pred, height, digitize_pred=False, sigmoid_pred=False, ax=None):
+    X1 = y_pred[:, 0]
+    X2 = y_pred[:, 1]
+    y_pred = y_pred[:, 2]
     y_pred = np.ma.masked_invalid(y_pred)
 
-    y_pred = np.digitize(y_pred, np.quantile(y_pred, [0, 0.2, 0.4, 0.6, 0.8, 1.0]))  # digitized values: 5 classes
 
-    print("predictions are in range [{0:.3f},{1:.3f}]".format(np.nanmin(y_pred), np.nanmax(y_pred)))
-    img = plt.imshow(y_pred.reshape((height, -1)), vmin=np.nanmin(y_pred), vmax=np.nanmax(y_pred))  # Should this be swapped? Even though it doesn't matter for square img
-    plt.set_cmap(colmap)
-    img.cmap.set_bad('tab:blue')
-    plt.show()
+    if digitize_pred:
+        y_pred = np.digitize(y_pred, np.quantile(y_pred, [0, 0.2, 0.4, 0.6, 0.8, 1.0]))  # digitized values: 5 classes
+    elif sigmoid_pred:
+        sigmoid = lambda x: 1 / (1 + np.exp(-.5 * (x - .5)))
+        y_pred = sigmoid(y_pred)
+        print("sigmoid transformed predictions are in range [{0:.3f},{1:.3f}]".format(np.nanmin(y_pred),
+                                                                                      np.nanmax(y_pred)))
+    else:
+        print("predictions are in range [{0:.3f},{1:.3f}]".format(np.nanmin(y_pred), np.nanmax(y_pred)))
 
-    sigmoid = lambda x: 1 / (1 + np.exp(-.5*(x-.5)))
-    y_pred = sigmoid(y_pred)
-    print("sigmoid transformed predictions are in range [{0:.3f},{1:.3f}]".format(np.nanmin(y_pred), np.nanmax(y_pred)))
-    img = plt.imshow(y_pred.reshape((height, -1)), vmin=np.nanmin(y_pred), vmax=np.nanmax(y_pred))
-    plt.set_cmap(colmap)
-    img.cmap.set_bad('tab:blue')
-    plt.show()
+    if ax is None:
+        fig, ax = plt.subplots()
+    # img = ax.imshow(y_pred.reshape((height, -1)), vmin=np.nanmin(y_pred), vmax=np.nanmax(y_pred))  # Should this be swapped? Even though it doesn't matter for square img
+    img = ax.imshow(y_pred.reshape((height, -1)), vmin=np.nanmin(y_pred),
+                     vmax=np.nanmax(y_pred), extent=[np.nanmin(X1), np.nanmax(X1), np.nanmin(X2), np.nanmax(X2)], aspect='auto')
+    set_colmap(img)
+    plt.show()  # Show original prediction
 
 
 def plot_colorbar():
