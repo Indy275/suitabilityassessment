b641a079615bddff089eb14bf121bb34d10b8033
Indy Dolmans <indy.dolmans@nelen-schuurmans.nl>
2023-07-14 16:57:47 +0200

    fixed bg plot with y-labels

--- plotting/plot_data.py
+++ plotting/plot_data.py
@@ -7,53 +7,49 @@ from shapely.geometry import Point
 from collections import Counter
 
 
-def plot_xy(X_train, y_train, X_test, y_test, train_h, train_w, test_h, test_w):
-    fig, ax = plt.subplots(2, 2)
-    ax[0, 0].imshow(y_train.reshape((train_h, train_w)))
-    ax[0, 0].set_title("Train labels")
+# def plot_xy(X_train, y_train, X_test, y_test, train_h, train_w, test_h, test_w):
+#     fig, ax = plt.subplots(2, 2)
+#     ax[0, 0].imshow(y_train.reshape((train_h, train_w)))
+#     ax[0, 0].set_title("Train labels")
+#
+#     ax[0, 1].imshow(X_train.reshape((train_h, train_w, X_train.shape[-1]))[:, :, 0])
+#     ax[0, 1].set_title("Train feature")
+#
+#     ax[1, 0].imshow(y_test.reshape((test_h, test_w)))
+#     ax[1, 0].set_title("Test labels")
+#
+#     ax[1, 1].imshow(X_test.reshape((test_h, test_w, X_test.shape[-1]))[:, :, 0])
+#     ax[1, 1].set_title("Test feature")
+#
+#     plt.tight_layout()
+#     plt.show()
 
-    ax[0, 1].imshow(X_train.reshape((train_h, train_w, X_train.shape[-1]))[:, :, 0])
-    ax[0, 1].set_title("Train feature")
 
-    ax[1, 0].imshow(y_test.reshape((test_h, test_w)))
-    ax[1, 0].set_title("Test labels")
-
-    ax[1, 1].imshow(X_test.reshape((test_h, test_w, X_test.shape[-1]))[:, :, 0])
-    ax[1, 1].set_title("Test feature")
-
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_row(y, bg, y_t, ax, i, h, w):
-    ax[i].imshow(bg, extent=[0, h, 0, w])
-    if y_t != 'Test':
-        indices = np.nonzero(y.reshape((h, w)))
-        ax[i].scatter(indices[1], np.abs(indices[0] - h), s=1, c='r')
-        posperc = len(y[y != 0]) / len(y) * 100
-
-        ax[i].set_title("{} labels: {} positive ({:.2f}%)".format(y_t, len(y[y != 0]), posperc))
-    ax[i].tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False)
-    ax[i].yaxis.set_ticklabels([])
+# def plot_row(y, bg, y_t, ax, i, h, w):
+#     ax[i].imshow(bg, extent=[0, h, 0, w])
+#     if y_t != 'Test':
+#         indices = np.nonzero(y.reshape((h, w)))
+#         ax[i].scatter(indices[1], np.abs(indices[0] - h), s=1, c='r')
+#         posperc = len(y[y != 0]) / len(y) * 100
+#
+#         ax[i].set_title("{} labels: {} positive ({:.2f}%)".format(y_t, len(y[y != 0]), posperc))
+#     ax[i].tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False)
+#     ax[i].yaxis.set_ticklabels([])
 
 
 def plot_y(loader, bg, ref_std):
     y = loader.y
-    lnglat = loader.lnglat
-    bbox = loader.bbox
-    print(bbox)
-    bbox = [float(i[0:-1]) for i in bbox.split()]
-    mod = loader.modifier
-    size = loader.size
+    lnglat = loader.lnglat[y != 0]
+    bbox = [float(i[0:-1]) for i in loader.bbox.split()]
+
     fig, ax = plt.subplots()
-    ax.imshow(bg, extent=[bbox[0], bbox[2], bbox[1], bbox[3]], origin='upper', alpha=0.8)
+    ax.imshow(bg, extent=[bbox[2], bbox[0], bbox[3], bbox[1]], origin='upper', alpha=0.8)
 
     if ref_std == 'hist_buildings':
-        indices = np.nonzero(y.reshape((size, size)))
-        ax.scatter(indices[1], np.abs(indices[0] - size), s=1, c='r')
+        ax.scatter(lnglat[:, 0], lnglat[:, 1], s=1, c='r')
     elif ref_std == 'expert_ref':
         cmap = plotting.plot.set_colmap()
-        points = [Point(x1, x2) for x1, x2 in lnglat[:, :2]]
+        points = [Point(x1, x2) for x1, x2 in loader.lnglat[:, :2]]
         point_counts = Counter(points)
         df = pd.DataFrame(
             [{'X1': p.x, 'X2': p.y, 'y': y[i], 'Count': point_counts[Point(p.x, p.y)]} for i, p in enumerate(points)])
@@ -61,12 +57,9 @@ def plot_y(loader, bg, ref_std):
         df = df.merge(df_mean, on=['X1', 'X2'], how='left')
         ax.scatter(df['X1'], df['X2'], c=df['y_mean'], cmap=cmap, s=df['Count'] * 25, edgecolors='black')
 
-    ax.set_title("{} labels: {} data points".format(mod, len(y[y != 0])))
+    ax.set_title("{} labels: {} data points".format(loader.modifier, len(y[y != 0])))
 
-    plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False)
-    ax.yaxis.set_ticklabels([])
     plt.show()
-
 
 # def plot_y_expert(loader, bg):
 #     lnglat = loader.lnglat
