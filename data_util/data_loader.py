76a5cafd19c3911bfe9b3d2587ca319f6aebe9a9
Indy Dolmans <indy.dolmans@nelen-schuurmans.nl>
2023-07-14 17:10:12 +0200

    code cleanup

--- data_util/data_loader.py
+++ data_util/data_loader.py
@@ -2,7 +2,6 @@ import os
 from joblib import load
 import configparser
 from pathlib import Path
-import matplotlib.image as mpimg
 import pandas as pd
 import numpy as np
 from PIL import Image
@@ -31,7 +30,6 @@ class DataLoader:
         self.X_orig = self.load_orig_df()
 
     def load_orig_df(self):
-        # load and reverse apply scaler
         ss = load(data_url + '/' + self.modifier + "/" + self.ref_std + "_scaler.joblib")
         df_orig = ss.inverse_transform(self.X)
         self.lnglat = df_orig[:, :2]
@@ -50,12 +48,12 @@ class DataLoader:
         df = pd.read_csv(data_url + '/' + self.modifier + '/' + self.ref_std + '.csv')
         col_names = list(df.columns)
         self.X = np.array(df.to_numpy()[:, :-1])
-        # self.lnglat = np.array(self.X[:, :2])
         y = df.to_numpy()[:, -1]
         if self.ref_std == 'hist_buildings':
-            y[y < 0] = 0
-            y[y > 0] = 1
-        y = np.where(np.isnan(y), 0, y)  # No house is built on NaN cells
+            y = np.where(y < 0, 0, np.where(y > 0, 1, np.where(np.isnan(y), 0, y)))
+            # y[y < 0] = 0
+            # y[y > 0] = 1
+        # y = np.where(np.isnan(y), 0, y)  # No house is built on NaN cells
         self.y = np.array(y)
         self.col_names = col_names[:-1]
 
@@ -89,42 +87,31 @@ class DataLoader:
 
     def load_bg(self):
         bg_png = data_url + '/' + self.modifier + '/bg.tif'
-        # bg_png = data_url + '/' + self.modifier + '/msk6.tiff'
-
         if not Path(bg_png).is_file():
             create_data.create_bg(self.modifier, self.bbox)
         bg = Image.open(bg_png)
         return bg
 
-    def get_xy(self):
-        return self.X, self.y, self.col_names
 
-    def get_y(self):
-        return self.y
-
-    def get_x(self):
-        return self.X
-
-
-def preprocess_input(train_loader, test_loader, lnglat=True):
-    X_train = train_loader.X
-    y_train = train_loader.y
-    col_names = train_loader.col_names
-
-    X_test = test_loader.X
-    lnglat = 0 if lnglat else 2  # 0 includes lng,lat. 2 is excluding.
-    trainLngLat = X_train[:, :2]
-    testLngLat = X_test[:, :2]
-
-    X_train = X_train[y_train != 0]
-    X_train = X_train[:, lnglat:]
-    y_train = y_train[y_train != 0]
-
-    test_nans = np.isnan(X_test).any(axis=1)
-    X_test = X_test[~test_nans]
-    X_test_feats = X_test[:, lnglat:]
-    col_names = col_names[lnglat:]
-    return X_train, y_train, X_test_feats, trainLngLat, testLngLat, test_nans, col_names
+# def preprocess_input(train_loader, test_loader, lnglat=True):
+#     X_train = train_loader.X
+#     y_train = train_loader.y
+#     col_names = train_loader.col_names
+#
+#     X_test = test_loader.X
+#     lnglat = 0 if lnglat else 2  # 0 includes lng,lat. 2 is excluding.
+#     trainLngLat = X_train[:, :2]
+#     testLngLat = X_test[:, :2]
+#
+#     X_train = X_train[y_train != 0]
+#     X_train = X_train[:, lnglat:]
+#     y_train = y_train[y_train != 0]
+#
+#     test_nans = np.isnan(X_test).any(axis=1)
+#     X_test = X_test[~test_nans]
+#     X_test_feats = X_test[:, lnglat:]
+#     col_names = col_names[lnglat:]
+#     return X_train, y_train, X_test_feats, trainLngLat, testLngLat, test_nans, col_names
 
 
 def load_meta(modifier):
--- plotting/plot_data.py
+++ plotting/plot_data.py
@@ -1,40 +1,9 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 import plotting.plot
 from shapely.geometry import Point
 from collections import Counter
-
-
-# def plot_xy(X_train, y_train, X_test, y_test, train_h, train_w, test_h, test_w):
-#     fig, ax = plt.subplots(2, 2)
-#     ax[0, 0].imshow(y_train.reshape((train_h, train_w)))
-#     ax[0, 0].set_title("Train labels")
-#
-#     ax[0, 1].imshow(X_train.reshape((train_h, train_w, X_train.shape[-1]))[:, :, 0])
-#     ax[0, 1].set_title("Train feature")
-#
-#     ax[1, 0].imshow(y_test.reshape((test_h, test_w)))
-#     ax[1, 0].set_title("Test labels")
-#
-#     ax[1, 1].imshow(X_test.reshape((test_h, test_w, X_test.shape[-1]))[:, :, 0])
-#     ax[1, 1].set_title("Test feature")
-#
-#     plt.tight_layout()
-#     plt.show()
-
-
-# def plot_row(y, bg, y_t, ax, i, h, w):
-#     ax[i].imshow(bg, extent=[0, h, 0, w])
-#     if y_t != 'Test':
-#         indices = np.nonzero(y.reshape((h, w)))
-#         ax[i].scatter(indices[1], np.abs(indices[0] - h), s=1, c='r')
-#         posperc = len(y[y != 0]) / len(y) * 100
-#
-#         ax[i].set_title("{} labels: {} positive ({:.2f}%)".format(y_t, len(y[y != 0]), posperc))
-#     ax[i].tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False)
-#     ax[i].yaxis.set_ticklabels([])
 
 
 def plot_y(loader, bg, ref_std):
@@ -60,26 +29,3 @@ def plot_y(loader, bg, ref_std):
     ax.set_title("{} labels: {} data points".format(loader.modifier, len(y[y != 0])))
 
     plt.show()
-
-# def plot_y_expert(loader, bg):
-#     lnglat = loader.lnglat
-#     bbox = loader.bbox
-#     y = loader.y
-#     mod = loader.modifier
-#     fig, ax = plt.subplots()
-#     cmap = plotting.plot.set_colmap()
-#     bbox = [float(i[0:-1]) for i in bbox.split()]
-#
-#     ax.imshow(bg, extent=[bbox[0], bbox[2], bbox[1], bbox[3]], origin='upper', alpha=0.8)
-#
-#     points = [Point(x1, x2) for x1, x2 in lnglat[:, :2]]
-#     point_counts = Counter(points)
-#     df = pd.DataFrame([{'X1': p.x, 'X2': p.y, 'y': y[i],'Count': point_counts[Point(p.x, p.y)]} for i, p in enumerate(points)])
-#     df_mean = df.groupby(['X1', 'X2'])['y'].mean().to_frame(name='y_mean').reset_index()
-#     df = df.merge(df_mean, on=['X1', 'X2'], how='left')
-#
-#     ax.scatter(df['X1'], df['X2'], c=df['y_mean'], cmap=cmap, s=df['Count']*25, edgecolors='black')
-#
-#     ax.set_title("{} labels: {} data points".format(mod, len(y[y != 0])))
-#     # ax.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False)
-#     plt.show()
