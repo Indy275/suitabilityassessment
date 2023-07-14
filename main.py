aaa0d19514ceedaa093d7813a4df21c0642f3a85
Indy Dolmans <indy.dolmans@nelen-schuurmans.nl>
2023-07-14 11:20:38 +0200

    now relies on metadata.csv to extract bbox etc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from data_util import create_data
+from data_util import create_data, data_loader
 from plotting.interactive_plot import DashApp
-from util import geospatial
 from models import run
 from data_analysis import process_expert_data
 
@@ -21,34 +20,6 @@ data_url = config['DEFAULT']['data_url']
 cluster = config['EXPERT_SETTINGS']['cluster']
 expert = config['EXPERT_SETTINGS']['expert']
 
-# todo: read bbox and size from metadata file
-bbox = {'schermerbeemster': '4.78729, 52.59679, 4.88033, 52.53982',
-        'purmer': '5.04805, 52.52374, 4.93269, 52.46846',
-        'purmerend': '4.90, 52.4800, 4.96284, 52.51857',
-        'volendam': '5.0168, 52.51664, 5.0853, 52.48006',
-        'volendamLowRes': '5.0168, 52.51664, 5.0853, 52.48006',
-        'oc': '4.8998, 52.4769, 5.0386, 52.5355',
-        'ocall': '4.8998, 52.4769, 5.0386, 52.5355',
-        'ws': '4.8281, 52.5892, 5.0457, 52.714',
-        'wsall': '4.8281, 52.5892, 5.0457, 52.714',
-        'tester': '4.8281, 52.5892, 5.0457, 52.714'}
-
-#todo deal with noordholland not having bbox but required in metadata
-if train_mod.startswith('noordholland') or test_mod.startswith('noordholland'):
-    HHNKbbox = geospatial.get_hhnk_bbox()
-    bbox['noordholland'] = HHNKbbox
-    bbox['noordhollandHiRes'] = HHNKbbox
-
-
-train_bbox = [float(i[0:-1]) for i in bbox[train_mod].split()]
-test_bbox = [float(i[0:-1]) for i in bbox[test_mod].split()]
-
-size = {'purmerend': 40, 'volendamLowRes': 40, 'volendam': 1000, 'ws': 1000,
-        'noordholland': 1000, 'noordhollandHiRes': 2000, 'tester': 200}
-
-train_size = size.get(train_mod, 400)
-test_size = size.get(test_mod, 400)
-
 if ml_model in ['gbr', 'gp']:
     ref_std = 'expert_ref'
 elif ml_model in ['svm', 'ocgp']:
@@ -58,6 +29,13 @@ elif ml_model == 'expert':
 else:
     assert ml_model in ['gbr', 'gp', 'svm', 'ocgp', 'expert'], f'ML model {ml_model} is invalid;' \
                                                      f' should be one of [gbr, gp, svm, ocgp, expert]'
+
+load_train = data_loader.DataLoader(train_mod, ref_std)
+load_test = data_loader.DataLoader(test_mod, ref_std='test_data')
+train_bbox = [float(i[0:-1]) for i in load_train.bbox.split()]
+test_bbox = [float(i[0:-1]) for i in load_test.bbox.split()]
+train_size = load_train.size
+test_size = load_test.size
 
 if process_expertdata:
     process_expert_data.run_model(cluster, expert_processing=expert)
